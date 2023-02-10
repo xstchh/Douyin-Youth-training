221831b54c0f3cd6e50448ffbcbdeff771ed4a0f
xstchh <1420239029@qq.com>
2023-02-10 16:17:34 +0800

    更新注释

--- socialize/friend.go
+++ socialize/friend.go
@@ -12,7 +12,7 @@ import (
 func FriendList(c context.Context, ctx *app.RequestContext) {
 	// 解析user id，TODO：正确性待测试
 	userId := c.Value("user_id")
-	// 根据userid从数据库中读取该id的粉丝列表
+	// 根据userid从数据库中读取该id的好友列表
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
