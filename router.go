f737b65f924460e84d72db51d65bcb58e2735d2c
xstchh <1420239029@qq.com>
2023-02-10 16:04:53 +0800

    router add follower and friend api

--- router.go
+++ router.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"Douyin-Youth-training/socialize"
+	"github.com/cloudwego/hertz/pkg/app/server"
+)
+
+func initrouter(h *server.Hertz) {
+	apiRouter := h.Group("/douyin")
+
+	// socialize
+	apiRouter.GET("/relation/follower/list/", socialize.FollowerList)
+	apiRouter.GET("/relation/friend/list/", socialize.FriendList)
+}
