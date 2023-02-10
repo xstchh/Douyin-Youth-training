37ad6c0d3c4d4bf9cf9522b80faabb2c8105091d
xstchh <1420239029@qq.com>
2023-02-10 16:01:50 +0800

    添加数据库接口

--- socialize/follower.go
+++ socialize/follower.go
@@ -2,11 +2,23 @@ package socialize
 
 import (
 	"Douyin-Youth-training/common"
+	"context"
 	"github.com/cloudwego/hertz/pkg/app"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"net/http"
 )
 
-func FollowerList(ctx *app.RequestContext, user common.User) {
+func FollowerList(c context.Context, ctx *app.RequestContext) {
+	// 解析user id，TODO：正确性待测试
+	userId := c.Value("user_id")
+	// 根据userid从数据库中读取该id的粉丝列表
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	var user common.User
+	db.First(&user, userId)
 	// 将要返回的请求体序列化为json格式
 	ctx.JSON(http.StatusOK, common.UserListResponse{
 		Response: common.Response{
