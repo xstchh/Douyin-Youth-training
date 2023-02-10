32eb36909525887f2a757d3504cb24d9f11c2953
xstchh <1420239029@qq.com>
2023-02-10 14:40:40 +0800

    add user userResponse Response in  common.go

--- common/common.go
+++ common/common.go
@@ -1,1 +1,20 @@
 package common
+
+type Response struct {
+	StatusCode int32  `json:"status_code"`
+	StatusMsg  string `json:"status_msg,omitempty"`
+}
+
+type UserListResponse struct {
+	Response
+	UserList []User `json:"user_list"`
+}
+
+type User struct {
+	Id            int64
+	Name          string
+	FollowCount   int64
+	FollowerCount int64
+	IsFollow      bool
+	FollowerList  []User // TODO:新添加粉丝列表属性，是否最优做法待商榷
+}
