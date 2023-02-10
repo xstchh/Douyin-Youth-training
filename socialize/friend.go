4a8c1acee086753988c2ccf656a2929a54d056f5
xstchh <1420239029@qq.com>
2023-02-10 14:43:25 +0800

    decoupling friend.go and common.go

--- socialize/friend.go
+++ socialize/friend.go
@@ -1,1 +1,16 @@
-package main
+package socialize
+
+import (
+	"Douyin-Youth-training/common"
+	"github.com/cloudwego/hertz/pkg/app"
+	"net/http"
+)
+
+func FriendList(ctx *app.RequestContext, user common.User) {
+	ctx.JSON(http.StatusOK, common.UserListResponse{
+		Response: common.Response{
+			StatusCode: 0,
+		},
+		UserList: user.FriendList,
+	})
+}
