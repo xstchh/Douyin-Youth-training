9b84fafc5e2417f3ff1b5c6f20af8adf43f68649
xstchh <1420239029@qq.com>
2023-02-10 14:41:36 +0800

    decoupling follower and common

--- socialize/follower.go
+++ socialize/follower.go
@@ -1,32 +1,15 @@
 package socialize
 
 import (
+	"Douyin-Youth-training/common"
 	"github.com/cloudwego/hertz/pkg/app"
 	"net/http"
 )
 
-type Response struct {
-	StatusCode int32  `json:"status_code"`
-	StatusMsg  string `json:"status_msg,omitempty"`
-}
-type UserListResponse struct {
-	Response
-	UserList []User `json:"user_list"`
-}
-
-type User struct {
-	Id            int64
-	Name          string
-	FollowCount   int64
-	FollowerCount int64
-	IsFollow      bool
-	FollowerList  []User // TODO:新添加粉丝列表属性，是否最优做法待商榷
-}
-
-func FollowerList(ctx *app.RequestContext, user User) {
+func FollowerList(ctx *app.RequestContext, user common.User) {
 	// 将要返回的请求体序列化为json格式
-	ctx.JSON(http.StatusOK, UserListResponse{
-		Response: Response{
+	ctx.JSON(http.StatusOK, common.UserListResponse{
+		Response: common.Response{
 			StatusCode: 0,
 		},
 		UserList: user.FollowerList,
