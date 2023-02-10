845ae48e9e593ebbe19028a228c1669d3f03ea15
Conqueror712 <2760644953@qq.com>
2023-02-10 14:13:01 +0800

    update user and feed with commment

---
+++ controller/user.go
@@ -0,0 +1,99 @@
+package controller
+
+import (
+	"net/http"
+	"sync/atomic"
+
+	"github.com/gin-gonic/gin"
+)
+
+// usersLoginInfo use map to store user info, and key is username+password for demo
+// user data will be cleared every time the server starts
+// test data: username=zhanglei, password=douyin
+var usersLoginInfo = map[string]User{
+	"zhangleidouyin": {
+		Id:            1,
+		Name:          "zhanglei",
+		FollowCount:   10,
+		FollowerCount: 5,
+		IsFollow:      true,
+	},
+}
+
+var userIdSequence = int64(1)
+
+type UserLoginResponse struct {
+	Response
+	UserId int64  `json:"user_id,omitempty"`
+	Token  string `json:"token"`
+}
+
+type UserResponse struct {
+	Response
+	User User `json:"user"`
+}
+
+// Register函数用于实现用户注册功能，会接受用户名和密码作为参数，并检查是否已经存在该用户，
+// 如果不存在则新建用户，并返回注册成功信息。
+func Register(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	token := username + password
+
+	if _, exist := usersLoginInfo[token]; exist {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "User already exist"},
+		})
+	} else {
+		atomic.AddInt64(&userIdSequence, 1)
+		newUser := User{
+			Id:   userIdSequence,
+			Name: username,
+		}
+		usersLoginInfo[token] = newUser
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 0},
+			UserId:   userIdSequence,
+			Token:    username + password,
+		})
+	}
+}
+
+// Login函数用于实现用户登录功能，会接受用户名和密码作为参数，并检查是否已经存在该用户，
+// 如果存在则返回登录成功信息。
+func Login(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	token := username + password
+
+	if user, exist := usersLoginInfo[token]; exist {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 0},
+			UserId:   user.Id,
+			Token:    token,
+		})
+	} else {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
+		})
+	}
+}
+
+// UserInfo函数用于查询用户的信息，会接受token作为参数，并检查是否存在该用户，
+// 如果存在则返回用户信息。
+func UserInfo(c *gin.Context) {
+	token := c.Query("token")
+
+	if user, exist := usersLoginInfo[token]; exist {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 0},
+			User:     user,
+		})
+	} else {
+		c.JSON(http.StatusOK, UserResponse{
+			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
+		})
+	}
+}
