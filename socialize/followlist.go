fe61fc0cc21ce005a1772bee714293ac6d317b55
ywbn <2363129320@qq.com>
2023-02-23 11:13:25 +0800

    sql

--- socialize/followlist.go
+++ socialize/followlist.go
@@ -3,13 +3,15 @@ package socialize
 import (
 	"log"
 	"sync"
+
+	"gorm.io/gorm"
 )
 
 type Follow struct {
-	Id         int64
-	UserId     int64
-	FollowerId int64
-	Cancel     int8
+	Id          int64
+	user_id     int64
+	follower_id int64
+	Cancel      int8
 }
 
 func (Follow) TableName() string {
@@ -31,7 +33,7 @@ func UniFollow() *FollowUser {
 		})
 	return followuser
 }
-func (*FollowUser,db *gorm) GetFollowingList(userId int64) ([]int64, error) {
+func (*FollowUser) GetFollowingList(userId int64, db *gorm.DB) ([]int64, error) {
 	var ids []int64
 	if err := db.Model(Follow{}).Where("follower.id = ?", userId).Pluck("user_id", &ids).Error; nil != err {
 		if "record not found" == err.Error() {
